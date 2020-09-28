87d07fc17e7e72118debbbaa601817b5a3949bf0
Yago Santos <yagossc@gmail.com>
2020-09-28 03:08:56 -0300

    Add build sequence

---
+++ build.el
@@ -0,0 +1,20 @@
+;; This file replaces itself with the actual configuration at first run.
+
+;; We can't tangle without org!
+(require 'org)
+
+;; Read seconf configuration
+(find-file (expand-file-name "seconf.org" user-emacs-directory))
+
+;; tangle it (only emacs-lisp source code)
+(org-babel-tangle nil nil 'emacs-lisp)
+
+;; rename: eos to init
+(rename-file
+ (expand-file-name "seconf.el" user-emacs-directory)
+ (expand-file-name "init.el" user-emacs-directory)
+ ;; OK-IF-ALREADY-EXISTS
+ t)
+
+;; finally byte-compile it
+(byte-compile-file (concat user-emacs-directory "init.el"))
