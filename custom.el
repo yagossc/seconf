1ffbb980832511ba7997f67d3b89b80b9df08a0c
Yago Santos <yagossc@gmail.com>
2023-08-27 12:37:43 -0300

    Update custom.el

--- custom.el
+++ custom.el
@@ -7,9 +7,12 @@
  '(company-dabbrev-downcase nil)
  '(company-dabbrev-ignore-case t)
  '(company-etags-ignore-case t)
+ '(custom-safe-themes
+   '("373fcd0ef7a142e4310766103f227194b7591526ea2a5da48c855986f4fe1616" default))
  '(menu-bar-mode nil)
- '(tool-bar-mode nil)
- '(scroll-bar-mode nil))
+ '(org-adapt-indentation t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
